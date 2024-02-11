5d251c9792c20d4ab4ad3fa63fe2deaeb8471afc
Irene <ireneherrero@outlook.com>
2024-02-11 22:23:02 +0100

    peticiones final

--- Proyecto_Meteorologia/Frontend/Recursos/peticiones.js
+++ Proyecto_Meteorologia/Frontend/Recursos/peticiones.js
@@ -1,5 +1,7 @@
+//Está variable recogerá la IP del servidor independiente si cambia o no
 var nombreHost = new URL (window.location.origin).hostname;
 
+//Función que recogerá los datos de registro y los guardara en la base de datos
 async function registro() {
 
   let inputnombre = document.getElementById('nombreRegister').value;
@@ -8,12 +10,14 @@ async function registro() {
   console.log(inputnombre + " " + inputemail + " " + inputcontrasena);
 
   try {
+    //Una vez recibidos todos los datos se enviaran al fecht
     const response = await fetch(`http://${nombreHost}:8083/api/registro`, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       },
+      //El cuerpo serán los datos que ha enviado el usuario 
       body: JSON.stringify({
         name: inputnombre,
         email: inputemail,
@@ -22,6 +26,7 @@ async function registro() {
     });
 
     if (response.ok) {
+      //Si el resultado es correcto solicitaremos el token de sesión del usuario y activaremos el formulario principal 
       const result = await response.json();
       console.log(result);
       localStorage.setItem('token', result.access_token);
@@ -30,6 +35,7 @@ async function registro() {
       $("#BodyLoginForm").css("display", "block");
       
     } else {
+      //Sino veremos que fallos ha habido en la petición 
       console.error('Registro fallido:', response.status);
 
     }
@@ -40,12 +46,14 @@ async function registro() {
 }
 
 
-
+//Está función verificará si el usuario está logueado 
 async function logueo() {
+  //Recogeremos todos los datos introducidos en el formulario de login
   let inputemail = document.getElementById('emailLogin').value;
   let inputcontrasena = document.getElementById('contrasenaLogin').value;
   console.log(inputemail + " " + inputcontrasena);
   try {
+    //El fect esperará hasta que se llenen todos los datos
     const response = await fetch(`http://${nombreHost}:8083/api/logueo`, {
       method: 'POST',
       headers: {
@@ -53,6 +61,7 @@ async function logueo() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
+        //Enviare los datos recogidos en el encabezado en el cuerpo de la solicitud
         email: inputemail,
         password: inputcontrasena
 
@@ -60,6 +69,7 @@ async function logueo() {
     });
 
     if (response.ok) {
+      //Si el logueo es correcto recogeremos el token de la sesión 
       const result = await response.json();
       console.log(result);
       localStorage.setItem('token', result.access_token);
@@ -69,15 +79,20 @@ async function logueo() {
 
     }
   } catch (err) {
+    //Sino nos dará detalles del error
     console.error(err);
   }
 }
-/*
+
+//Está función cerrará la sesión del usuario (estado producción)
+
 async function CierreSesion() {
+  //Recibirá el token del usuario
   var tokenUsuario = localStorage.getItem('token');
 
   const options = {
     method: 'GET',
+    //Lo enviaremos en el encabezado 
     headers: {
       Authorization: `Bearer ${tokenUsuario}`
     }
@@ -85,15 +100,18 @@ async function CierreSesion() {
 
 
   
-    //Lugares Selecionados por el usuario
+    //El  esperará a recibir el token para cerrar la sesión 
     const response = await fetch(`http://${nombreHost}:8083/api/cerrarSesion`, options)
     if (response.ok) {
       const result = await response.json();
       console.log(result);
+      //Desactivaremos el formulario y el de resultados y activaremos la página de bienvenida 
       $("#BodyPortada").css("display", "block");
       $("#BodyLoginForm").css("display", "none");
+      $("#bodyLoginResultados").css("display", "none");
+      //Por último limpiaremos el local storage 
       localStorage.clear();
     }
-}*/
+}
 
 

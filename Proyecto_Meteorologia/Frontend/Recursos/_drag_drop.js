304c6085bb75daf7a99e93f9b1c640e217a4dc78
Irene <ireneherrero@outlook.com>
2024-02-11 09:15:35 +0100

    SubidaDragDrop.js

--- Proyecto_Meteorologia/Frontend/Recursos/_drag_drop.js
+++ Proyecto_Meteorologia/Frontend/Recursos/_drag_drop.js
@@ -41,55 +41,71 @@ $(document).ready(function dragDrop() {
         objeto = event.target.id;
     });
 
-    // Permitir que el destino reciba elementos arrastrados
+    // Permitir que el contenedor de items desactivados  reciba elementos arrastrados
     $("#items_Desactivados").on('dragover', function (event) {
         //console.log(event.target.id);
 
+        //Si el item se está moviendo sobre items desactivados el fondo del contenedor de ítems desactivados tornará a rojo
         $("#items_Desactivados").css("background-color", "rgb(157, 6, 6)");
+
+        //Característica principal para que el item pueda ser dragable en el contenedor de ítems desactivados 
         event.preventDefault();
 
 
     });
+
+    // Permitir que el contenedor de ítems activados reciba elementos arrastrados
     $("#items_Activados").on('dragover', function (event) {
 
         //console.log(event.target.id);
 
+        //Si el item se está moviendo sobre items activados, el fondo del contenedor de ítems activados tornará a azul
         $("#items_Activados").css("background-color", "rgb(7, 7, 87)");
+
+        //Característica principal para que el item pueda ser dragable en el contenedor de ítems activados 
         event.preventDefault();
 
 
     });
 
 
-
+    /*Cuando el item deje de estar sobre el contenedor, ya sea en el contenedor 
+    de ítems desactivados o el de ítems activados el contenedor tornará a blanco semi opaco*/ 
     $("#items_Activados, #items_Desactivados").on('dragleave', function (event) {
         $(this).css("background-color", "rgba(255, 255, 255, 0.650)");
     });
 
-    // Manejar la acción de soltar el elemento
+    // Manejar la acción de soltar el elemento ya sea en el contenedor de ítems desactivados o el de ítems activados 
     $("#items_Desactivados, #items_Activados").on('drop', function (event) {
 
+        //Cuando un item se suelte en el contenedor de ítems activados o desactivados el fondo tornará a blanco semi opaco 
         $("#items_Activados,#items_Desactivados").css("background-color", "rgba(255, 255, 255, 0.650)");
-        var data = event.originalEvent.dataTransfer.getData("text/plain");
 
+        //Recogemos el id del elemento soltado en el contenedor
+        var data = event.originalEvent.dataTransfer.getData("text/plain")
         var draggedElement = document.getElementById(data);
 
-
+        //En un item no se pueda soltar sobre otro ítem 
         if (event.target !== draggedElement) {
             // Agregar el elemento al área general
             $(this).append(draggedElement);
 
-            // Actualizar el conjunto de IDs sin duplicados
+            // Actualizar el conjunto de IDs sin duplicados en contenedor de ítems activados y desactivados 
+
+            // Si dejamos un item en el contenedor de ítems activados se añadirá al set de ítemsActivadosUnicos
             $("#items_Activados").children().each(function () {
                 var idElemento = $(this).attr('id');
                 elementosActivadosUnicos.add(idElemento);
             });
+
+            // Si dejamos un item en el contenedor de  ítems desactivados, se eliminará del set de ítemsActivadosUnicos
             $("#items_Desactivados").children().each(function () {
                 var idElemento = $(this).attr('id');
                 elementosActivadosUnicos.delete(idElemento);
             });
 
-
+         
+            //Devolveremos los items activados únicos ya filtrados 
             console.log(elementosActivadosUnicos);
             return (elementosActivadosUnicos);
         }
